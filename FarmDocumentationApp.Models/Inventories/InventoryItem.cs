df77ab3c84f81a303e96b16768aaf36148917e0c
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2023-07-24 15:20:58 +0200

    wip

---
+++ FarmDocumentationApp.Models/Inventories/InventoryItem.cs
@@ -0,0 +1,12 @@
+﻿using FarmDocumentationApp.Core.Ids;
+
+namespace FarmDocumentationApp.Models.Inventories;
+public class InventoryItem
+{
+    public required FarmId FarmId { get; init; }
+    // TODO:
+    // Hay / silage
+    // 1st / 2nd (...) cut
+    // Crop
+    // 
+}
